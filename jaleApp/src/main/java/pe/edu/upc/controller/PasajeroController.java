342edb685ee80683b21ce2c12aba00bd782573a9
Brayan1951 <72108734+Brayan1951@users.noreply.github.com>
2021-06-15 01:12:30 -0500

    Add files via upload

--- jaleApp/src/main/java/pe/edu/upc/controller/PasajeroController.java
+++ jaleApp/src/main/java/pe/edu/upc/controller/PasajeroController.java
@@ -1,6 +1,9 @@
 package pe.edu.upc.controller;
 
+import java.text.ParseException;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -10,10 +13,12 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.support.SessionStatus;
+
 
 import pe.edu.upc.entities.Pasajero;
 import pe.edu.upc.service.IPasajeroService;
@@ -73,4 +78,63 @@ public class PasajeroController {
 
 		return "redirect:/passengers/list";
 	}
+	
+	
+	@GetMapping("/detalle/{id}")
+	public String viewPassenger( @PathVariable(value = "id") int id,Model model) {
+		try {
+			Optional<Pasajero>pasajero=pService.listarid(id);
+			if (!pasajero.isPresent()) {
+				model.addAttribute("mensaje","Pasajero no existe!!");
+				return "redirect:/passengers/list";
+			}else {
+				model.addAttribute("passenger",pasajero.get());
+				return "passenger/updatePassenger";
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			
+		}
+		
+		return "passenger/updatePassenger";
+		
+	}
+	
+	@GetMapping("/listFind")
+	public String listCategoriesFind(Model model) {
+		try {
+			model.addAttribute("passenger", new Pasajero());
+			model.addAttribute("listaPasajeros", pService.list());
+		} catch (Exception e) {
+			model.addAttribute("error", e.getMessage());
+		}
+		return "/passenger/find";
+	}
+	
+	@RequestMapping("/find")
+	public String findbyMedice(Model model,@ModelAttribute Pasajero pass) throws ParseException{
+		
+		model.addAttribute("passenger", new Pasajero());
+		List<Pasajero> listpass;
+		
+		pass.setNamePasajero(pass.getNamePasajero());
+		listpass=pService.findByname(pass.getNamePasajero());
+		
+		if(listpass.isEmpty()) {
+			listpass=pService.findBynamePasajeroLikeIgnoreCase(pass.getNamePasajero());
+			
+			
+		}
+		if(listpass.isEmpty()) {
+			
+			model.addAttribute("mensaje","No se encontró");
+			
+		}
+		
+		model.addAttribute("listaPasajeros", listpass);
+		return "/passenger/find";
+		
+	}
+	
+	
 }
---
+++ jaleApp/src/main/java/pe/edu/upc/controller/RolUserController.java
@@ -0,0 +1,16 @@
+package pe.edu.upc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.edu.upc.service.IRolUserService;
+
+@Controller
+@RequestMapping("/roluser")
+public class RolUserController {
+
+	private IRolUserService ruService;
+	
+	
+	
+}
